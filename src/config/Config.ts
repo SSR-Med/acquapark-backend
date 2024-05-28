d426593b65adf85c682ec8396a9683062d8fea58
SSR-Med <sasalazarr@unal.edu.co>
2024-05-28 12:50:54 -0500

    Feature(Changes to connection db)

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const port = process.env.PORT;
-export const database_url = process.env.DATABASE_URL;
+// Database
+export const database_name = process.env.DATABASE_NAME;
+export const database_user = process.env.DATABASE_USER;
 export const database_host = process.env.DATABASE_HOST;
+export const database_password = process.env.DATABASE_PASSWORD;
+export const database_port = Number(process.env.DATABASE_PORT);
 export const salt = Number(process.env.SALT);
 export const jwt_key = process.env.JWT_KEY;
--- src/config/Database.ts
+++ src/config/Database.ts
@@ -2,12 +2,21 @@
 const {Sequelize} = require('sequelize');
 import { time } from "console";
 // Env variables
-import { database_host, database_url } from "./Config";
+import { database_name, database_host, database_password, database_port, database_user} from "./Config";
 
-export const database = new Sequelize(database_url, {
+export const database = new Sequelize({
     host: database_host,
+    database: database_name,
+    username: database_user,
+    password: database_password,
+    port: database_port,
     dialect: 'postgres',
     logging: false,
+    "dialectOptions": {
+      "ssl":{
+        "require": true
+      }
+    },
     define: {
       timestamps: false
     },
